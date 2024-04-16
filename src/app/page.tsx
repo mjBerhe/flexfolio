c89a968146a4b11b3a40eb75ec70dc75af5c9e08
mjBerhe <mjberhe123@hotmail.com>
2024-04-16 15:51:29 -0400

    readme + topnav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div>Hello GAINGRAPH</div>
+    <main className="flex flex-col items-center justify-center text-white">
+      <div>FlexFolio</div>
     </main>
   );
 }
