ff6dd79341d886df19428e93d78c980ad4a2288f
mjBerhe <mjberhe123@hotmail.com>
2024-04-17 13:01:08 -0400

    force dynamic + typescript version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import { db } from "~/server/db";
 
+// every db change updates this page
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   const posts = await db.query.posts.findMany();
   console.log(posts);
