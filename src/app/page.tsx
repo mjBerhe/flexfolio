d0096634ac69b697b5c40ffb1a739cda5fcefee8
mjBerhe <mjberhe123@hotmail.com>
2024-04-24 12:32:34 -0400

    redirects to dashboard after login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
-import Link from "next/link";
-import { db } from "~/server/db";
-import { getMyWorkouts, createWorkout } from "~/server/queries";
-
-import { Button } from "./_components/ui/button";
-import { Calendar } from "./_components/calendar";
-import { SignedIn, SignedOut } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 // every db change updates this page
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const workouts = await getMyWorkouts();
-  // console.log(workouts);
+  const { userId } = auth();
+  if (userId) {
+    redirect("/dashboard");
+  }
 
   return (
     <main className="flex w-full flex-grow flex-col text-white">
